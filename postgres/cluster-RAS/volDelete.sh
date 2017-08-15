961d0932eeee120584fac32e37ea4fb16b012860
ArvindGupta <agupta@datawisesystems.com>
2017-08-15 11:21:20 -0700

    use copied version of kubectl in to images instead of downloading on fly.

--- postgres/cluster-RAS/volDelete.sh
+++ postgres/cluster-RAS/volDelete.sh
@@ -3,6 +3,6 @@ dctl volume delete pg-vol-0 -y
 dctl volume delete pg-vol-1 -y
 dctl volume delete pg-vol-2 -y
 
-dctl volume delete consul-vol-0 -y 
-dctl volume delete consul-vol-1 -y
-dctl volume delete consul-vol-2 -y
+#dctl volume delete consul-vol-0 -y 
+#dctl volume delete consul-vol-1 -y
+#dctl volume delete consul-vol-2 -y
