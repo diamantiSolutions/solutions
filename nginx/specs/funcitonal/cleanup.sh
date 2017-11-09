e860e66a9d23938b2708e05e5514e2299a0e0cc0
ArvindGupta <agupta@datawisesystems.com>
2017-11-09 15:31:40 -0800

    updated run scripts for normal nginx(not plus)

--- nginx/specs/funcitonal/cleanup.sh
+++ nginx/specs/funcitonal/cleanup.sh
@@ -1,9 +1,15 @@
-kubectl delete -f nginx-ingress-rc.yaml
+kubectl delete -f nginx-ingress-rc.yaml --namespace=$1
 
-kubectl delete -f coffee-configmap.yaml 
+kubectl delete -f coffee-configmap.yaml --namespace=$1
 
-kubectl delete -f coffee-rc.yaml
-kubectl delete -f coffee-svc.yaml
+kubectl delete -f coffee-rc.yaml --namespace=$1
+kubectl delete -f coffee-svc.yaml --namespace=$1
 
-kubectl delete -f cafe-secret.yaml
-kubectl delete -f cafe-ingress.yaml
+kubectl delete -f tea-configmap.yaml  --namespace=$1
+
+kubectl delete -f tea-rc.yaml --namespace=$1
+kubectl delete -f tea-svc.yaml --namespace=$1
+
+
+kubectl delete -f cafe-secret.yaml --namespace=$1
+kubectl delete -f cafe-ingress.yaml --namespace=$1
