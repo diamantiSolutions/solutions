6bb3b4eb6254cbf3d595b9072668de0de5eb0db6
ArvindGupta <agupta@datawisesystems.com>
2018-02-07 12:11:20 -0800

    microservice example.

--- mongodb-cluster/specs/ss/run.sh
+++ mongodb-cluster/specs/ss/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #date "+%H:%M:%S"
 
-MY_NAME="demo2"
+MY_NAME=${1:-"demo"}
 
 ./cleanup.sh $MY_NAME 
 sleep 20
@@ -11,5 +11,5 @@ sed  -e "s/SVC_NAME/$MY_NAME/g" mongo-statefulset.yaml | kubectl create -f -
 
 sleep 80
 
-sed  -e "s/TARGET/$MY_NAME-rs-0.$MY_NAME-mongo,$MY_NAME-rs-1.$MY_NAME-mongo,$MY_NAME-rs-2.$MY_NAME-mongo/g"  mongo-bench.json | kubectl create -f -
+#sed  -e "s/TARGET/$MY_NAME-rs-0.$MY_NAME-mongo,$MY_NAME-rs-1.$MY_NAME-mongo,$MY_NAME-rs-2.$MY_NAME-mongo/g"  mongo-bench.json | kubectl create -f -
 
