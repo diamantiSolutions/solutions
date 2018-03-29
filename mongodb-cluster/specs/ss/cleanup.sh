b65c87f4370ce0bd17cb7084443b15d9edc1a7a5
ArvindGupta <agupta@diamanti.com>
2018-03-29 11:31:58 -0700

    mongodb minor fixes

--- mongodb-cluster/specs/ss/cleanup.sh
+++ mongodb-cluster/specs/ss/cleanup.sh
@@ -5,4 +5,4 @@ MY_NAME=$1
 
 kubectl delete -f diamanti-m3-high.yaml  
 sed  -e "s/SVC_NAME/$MY_NAME/g" mongo-statefulset.yaml | kubectl delete -f -
-sed  -e "s/TARGET/$MY_NAME-rs-0.$MY_NAME-mongo,$MY_NAME-rs-1.$MY_NAME-mongo,$MY_NAME-rs-2.$MY_NAME-mongo/g"  mongo-bench.json | kubectl delete -f -
+sed  -e "s/SVC_NAME/$MY_NAME/g"  -e "s/TARGET/$MY_NAME-rs-0.$MY_NAME-mongo,$MY_NAME-rs-1.$MY_NAME-mongo,$MY_NAME-rs-2.$MY_NAME-mongo/g"  mongo-bench.json | kubectl delete -f -
