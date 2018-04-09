a8981769a5fc4fad6656740867f686f394a3ce49
ArvindGupta <agupta@diamanti.com>
2018-04-09 14:02:40 -0700

    merging github changes to gitlab

--- nginx/specs/perf/cleanup.sh
+++
@@ -1,9 +0,0 @@
-kubectl delete -f nginx-ingress-rc.yaml
-
-kubectl delete -f coffee-configmap.yaml 
-
-kubectl delete -f coffee-rc.yaml
-kubectl delete -f coffee-svc.yaml
-
-kubectl delete -f cafe-secret.yaml
-kubectl delete -f cafe-ingress.yaml
